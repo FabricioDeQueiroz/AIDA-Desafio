cb5b66b60decaf03e002dfd1e673dfb9419b1107
FabricioDeQueiroz <fabriciodequeiroz2016@gmail.com>
2026-03-03 11:24:23 -0300

    refactor(backend): migration for nationality column in Author table to allow null values

---
+++ Library-Service/Migrations/20260303022159_UpdatedModels.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Library_Service.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "nationality",
+                table: "Author",
+                type: "character varying(2)",
+                maxLength: 2,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(2)",
+                oldMaxLength: 2);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "nationality",
+                table: "Author",
+                type: "character varying(2)",
+                maxLength: 2,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(2)",
+                oldMaxLength: 2,
+                oldNullable: true);
+        }
+    }
+}
