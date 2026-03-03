7c7a701522ca9b58b61d76d48400e181d21fc837
FabricioDeQueiroz <fabriciodequeiroz2016@gmail.com>
2026-03-03 16:14:46 -0300

    feat(backend): implementation of loan controller, and new migration

---
+++ Library-Service/Migrations/20260303190846_AdjustedModels.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Library_Service.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdjustedModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "isbn",
+                table: "Book",
+                type: "character varying(13)",
+                maxLength: 13,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(17)",
+                oldMaxLength: 17);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "isbn",
+                table: "Book",
+                type: "character varying(17)",
+                maxLength: 17,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(13)",
+                oldMaxLength: 13);
+        }
+    }
+}
