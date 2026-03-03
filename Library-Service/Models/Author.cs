2e9642b78a3f27319ba65763f81a0ffe1f48c279
FabricioDeQueiroz <fabriciodequeiroz2016@gmail.com>
2026-03-02 23:13:01 -0300

    feat(backend): add author service for CRUD operations

--- Library-Service/Models/Author.cs
+++ Library-Service/Models/Author.cs
@@ -15,7 +15,7 @@ public class Author : BaseEntity
 
     [StringLength(2, ErrorMessage = "A nacionalidade não pode exceder 2 caracteres.")]
     [Column("nationality")]
-    public string Nationality { get; set; } = string.Empty;
+    public string? Nationality { get; set; } = string.Empty;
 
     public ICollection<Book> Books { get; set; } = new List<Book>();
 }
